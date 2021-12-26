3301572430b3159bccc76a60c3e87e1990c69b7a
Alexander Korobov <korobass1987@yandex.ru>
2021-12-26 23:14:41 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,15 +7,15 @@ const galerry = document.querySelector(".gallery")
 
 const galerryItm = galleryItems.map((item) =>  
     `<a class="gallery__item" href="${item.original}">
-  <img class="gallery__image" src="${item.preview}"  alt="${item.description}" title="${item.description}" />
+  <img class="gallery__image" src="${item.preview}"  alt="${item.description}"   />
 </a>`
 ).join('')
 
 galerry.insertAdjacentHTML('afterbegin', galerryItm)
-let gallery = new SimpleLightbox('.gallery a');
-gallery.defaultOptions.captionDelay = 250
-gallery.defaultOptions.captionData = "alt"
-console.log(gallery.defaultOptions.captionData)
+let gallery = new SimpleLightbox('.gallery a', {captionsData: "alt", captionDelay: 250});
+
+
+console.log(gallery.defaultOptions.captionsData)
 
 gallery.on('show.simplelightbox', function (event) {
          event.preventDefault();
